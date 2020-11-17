b03d78fd0b82c36db4f35079b206a38ed55ce4ef
William Newman <38147848+william-newman@users.noreply.github.com>
2020-11-17 16:48:33 -0700

    Cleaned up, has full library now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { swears, lesserSwears, deplorable } from '../assets/swears.js';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,10 @@ export class AppComponent {
   mainInputText = '';
   isObscene = false;
   isQuestionable = false;
+  totalObsceneWordsFound = 0;
+  silenced = false;
+  willSilence = false;
+  silenceCounter = 0; // Persist per account
 
   constructor() { }
 
@@ -26,10 +31,21 @@ export class AppComponent {
   checkObscene(chatInput: string) {
     this.isObscene = false;
     this.isQuestionable = false;
+    this.totalObsceneWordsFound = 0;
 
-    if (chatInput.includes("fuck")) {
-      this.isObscene = true;
+    if (chatInput.includes(deplorable)) {
+      this.willSilence = true;
+    } else {
+      this.willSilence = false;
     }
+
+    swears.forEach(word => {
+      if (chatInput.includes(word)) {
+        this.isObscene = true;
+        this.totalObsceneWordsFound++;
+        return;
+      }
+    });
 
     if (!this.isObscene) {
       this.checkQuestionable(chatInput);
@@ -37,9 +53,70 @@ export class AppComponent {
   }
 
   checkQuestionable(chatInput: string) {
-    if (chatInput.includes("butts")) {
-      this.isQuestionable = true;
+    lesserSwears.forEach(word => {
+      if (chatInput.includes(word)) {
+        this.isQuestionable = true;
+      }
+    });
+  }
+
+  checkForSend(event: KeyboardEvent) {
+    if (event.ctrlKey && event.key === 'Enter') {
+      this.sendChatMessage();
     }
   }
 
+  sendChatMessage() {
+    if (this.totalObsceneWordsFound > 9 || this.willSilence) {
+      this.silenced = true;
+    }
+
+    if (this.silenced) {
+      this.silenceCounter++;
+      this.silenceTimeout();
+    }
+
+    this.mainInputForm.reset();
+  }
+
+  silenceTimeout() {
+    let silenceTimer = 3000;
+
+    console.log(
+      this.silenceCounter
+    );
+
+    if (this.silenceCounter < 9) {
+      if (this.silenceCounter < 3) {
+        silenceTimer == 3000
+      } else {
+        switch (this.silenceCounter) {
+          case 3:
+            silenceTimer = 5000;
+            break;
+
+          case 4:
+            silenceTimer = 7500;
+            break
+
+          case 5:
+            silenceTimer = 10000;
+            break
+
+          default:
+            silenceTimer = 30000;
+            break;
+        }
+      }
+      setTimeout(() => this.liftSilence(), silenceTimer);
+    } else {
+      // ban
+    }
+    console.log(silenceTimer);
+
+  }
+
+  liftSilence() {
+    this.silenced = false;
+  }
 }
